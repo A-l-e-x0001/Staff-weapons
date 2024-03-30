409cd4e881309f63563a1963d349fb989d5f03e8
Alex <119552561+A-l-e-x0001@users.noreply.github.com>
2024-03-30 02:09:30 -0500

    Update SGW.lua

--- SGW.lua
+++ SGW.lua
@@ -1,6 +1,6 @@
 RegisterServerEvent("StaffW:perms")
 AddEventHandler("StaffW:perms", function(playerPed)
-    if IsPlayerAceAllowed(source, "Administrator") then --Change "Administrator" to what your likeing
+    if IsPlayerAceAllowed(source, "Perms.Staffw") then --add Perms.Staffw to your ace permissions
         TriggerClientEvent('StaffW:HasPerms', source)
     else
         TriggerClientEvent('StaffW:NoPerms', source)
